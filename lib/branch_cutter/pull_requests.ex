23634045f4b05c30b197f675e2f2eda94684af31
smaximov <s.maximov@teachmeplease.com>
2018-06-25 18:25:08 +0300

    Add PullRequests.delete/3

--- lib/branch_cutter/pull_requests.ex
+++ lib/branch_cutter/pull_requests.ex
@@ -13,9 +13,17 @@ defmodule BranchCutter.PullRequests do
     |> Repo.insert(on_conflict: :nothing)
   end
 
+  @spec count(String.t(), String.t()) :: integer()
   def count(repo, branch) do
     PullRequest
     |> Query.where(repo: ^repo, branch: ^branch)
     |> Repo.aggregate(:count, :number)
+  end
+
+  @spec delete(String.t(), String.t(), integer()) :: {integer(), nil} | no_return()
+  def delete(repo, branch, number) do
+    PullRequest
+    |> Query.where(repo: ^repo, branch: ^branch, number: ^number)
+    |> Repo.delete_all()
   end
 end
--- test/branch_cutter/pull_requests_test.exs
+++ test/branch_cutter/pull_requests_test.exs
@@ -2,10 +2,9 @@ defmodule BranchCutter.PullRequestsTest do
   use BranchCutter.RepoCase, async: true
 
   alias BranchCutter.PullRequests
+  alias BranchCutter.Schema.PullRequest
 
   describe "insert/3" do
-    alias BranchCutter.Schema.PullRequest
-
     import PullRequests, only: [insert: 3, count: 2]
 
     test "it is successful with valid values and duplicates" do
@@ -32,6 +31,21 @@ defmodule BranchCutter.PullRequestsTest do
                branch: ["can't be blank"],
                number: ["can't be blank"]
              } = errors_on(changeset)
+    end
+  end
+
+  describe "delete/3" do
+    import PullRequests, only: [insert: 3, count: 2, delete: 3]
+
+    test "it deletes a pull request" do
+      repo = "repo"
+      branch = "branch"
+      number = 42
+
+      assert {:ok, _} = insert(repo, branch, number)
+      assert {1, nil} = delete(repo, branch, number)
+      assert count(repo, branch) == 0
+      assert {0, nil} = delete(repo, branch, number)
     end
   end
 end
