eb750db1fcc9715ab7bbecadd2ccd3368b41dc81
smaximov <s.maximov@teachmeplease.com>
2018-06-26 13:43:39 +0300

    Don't delete a branch if it has at least one open pull request

--- lib/branch_cutter/plug/payload.ex
+++ lib/branch_cutter/plug/payload.ex
@@ -3,6 +3,8 @@ defmodule BranchCutter.Plug.Payload do
 
   import Plug.Conn
 
+  alias BranchCutter.PullRequests
+
   @impl Plug
   def init(opts), do: opts
 
@@ -18,21 +20,50 @@ defmodule BranchCutter.Plug.Payload do
          "pull_request",
          %{
            "action" => "closed",
+           "number" => number,
            "pull_request" => %{
              "head" => %{"ref" => branch, "repo" => %{"full_name" => repo_slug}}
            }
          },
          conn
        ) do
-    if protected_branch?(branch) do
-      require Logger
+    PullRequests.delete(repo_slug, branch, number)
 
-      Logger.warn(fn ->
-        "Skip deleting protected branch #{branch} on #{repo_slug}"
-      end)
-    else
-      spawn(BranchCutter.Handler, :pull_request_closed, [repo_slug, branch])
+    require Logger
+
+    cond do
+      protected_branch?(branch) ->
+        Logger.warn(fn ->
+          "Skip deleting protected branch #{branch} on #{repo_slug}"
+        end)
+
+      PullRequests.count(repo_slug, branch) > 0 ->
+        Logger.warn(fn ->
+          "Skip deleting branch #{branch} on #{repo_slug} because it has open pull requests"
+        end)
+
+      true ->
+        spawn(BranchCutter.Handler, :pull_request_closed, [repo_slug, branch])
     end
+
+    send_resp(conn, 200, "")
+  end
+
+  @open_actions ~w[opened reopened]
+
+  defp handle_payload(
+         "pull_request",
+         %{
+           "action" => action,
+           "number" => number,
+           "pull_request" => %{
+             "head" => %{"ref" => branch, "repo" => %{"full_name" => repo_slug}}
+           }
+         },
+         conn
+       )
+       when action in @open_actions do
+    PullRequests.insert(repo_slug, branch, number)
 
     send_resp(conn, 200, "")
   end
